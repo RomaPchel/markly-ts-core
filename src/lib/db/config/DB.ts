f659fea932edb59244ea377acce199c8f5fb979b
romapchel <notpchelintsevroma@gmail.com>
2025-04-10 11:57:02 +0300

    updated entities path

--- src/lib/db/config/DB.ts
+++ src/lib/db/config/DB.ts
@@ -1,10 +1,5 @@
 import { MikroORM, EntityManager } from "@mikro-orm/postgresql";
 import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
-import * as path from "path";
-import {fileURLToPath} from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export class Database {
   private static instance: Database;
@@ -20,7 +15,7 @@ export class Database {
     if (!Database.instance) {
       const orm = await MikroORM.init({
         metadataProvider: TsMorphMetadataProvider,
-        entities: [path.join(__dirname, "../entities")],
+        entities: ["./dist/lib/entities/*.js"],
         dbName: process.env.DATABASE_NAME || "saas",
         host: process.env.DATABASE_HOST || "localhost",
         port: Number(process.env.DATABASE_PORT) || 5432,
