d297d0a6d8b05befeab3db409d80bba9479bc87b
romapchel <notpchelintsevroma@gmail.com>
2025-05-22 11:10:31 +0300

    pub sub different environments 1.2.9

--- src/lib/classes/PubSub.ts
+++ src/lib/classes/PubSub.ts
@@ -7,15 +7,16 @@ export class PubSubWrapper {
     private static pubsub = new PubSub({ projectId: 'saas-452909' });
 
     static async publishMessage<T = any>(topicName: string, message: T): Promise<string> {
+        const environmentTopic = `${process.env.ENVIRONMENT}-${topicName}`
         const dataBuffer = Buffer.from(JSON.stringify(message));
-        const topic: Topic = PubSubWrapper.pubsub.topic(topicName);
+        const topic: Topic = PubSubWrapper.pubsub.topic(environmentTopic);
 
         try {
             const messageId = await topic.publishMessage({ data: dataBuffer });
-            logger.info(`Published to ${topicName}: message ID ${messageId}`);
+            logger.info(`Published to ${environmentTopic}: message ID ${messageId}`);
             return messageId;
         } catch (err: any) {
-            logger.error(`Failed to publish to ${topicName}: ${err.message}`);
+            logger.error(`Failed to publish to ${environmentTopic}: ${err.message}`);
             throw err;
         }
     }
@@ -25,7 +26,9 @@ export class PubSubWrapper {
         onMessage: (msg: T) => Promise<void>,
         onError?: (err: Error) => void
     ): Subscription {
-        const subscription: Subscription = this.pubsub.subscription(subscriptionName);
+        const environmentSubscription = `${process.env.ENVIRONMENT}-${subscriptionName}`
+
+        const subscription: Subscription = this.pubsub.subscription(environmentSubscription);
 
         subscription.on('message', async (message) => {
             try {
@@ -40,7 +43,7 @@ export class PubSubWrapper {
         });
 
         subscription.on('error', (err) => {
-            logger.error(`Subscription ${subscriptionName} failed: ${err.message}`);
+            logger.error(`Subscription ${environmentSubscription} failed: ${err.message}`);
             onError?.(err);
         });
 
